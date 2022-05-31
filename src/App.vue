bd4cf61594bcf618d082dc67fe910052077ec357
CarlosGuerrero9909 <caraguerrerob@correo.udistrital.edu.co>
2022-05-31 13:37:43 -0500

    implementacion-directivas

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="div">
-    <h1>Saludo: {{mensaje}}</h1>
-    <h2 :class="clasePruebas" v-text="texto"></h2>
-    <h3>--> {{inputText}}</h3>
-    <p v-html="textoHtml"></p>
-    <input type="text" v-model="inputText"><br>
-    <img :src="img" alt=""> <!-- : = v-bind: -->
+  <div class="div-content">
+    <h1 id="titulo">Titulo: {{message}}</h1> <!-- interpolacion de texto -->
+    <div id="div_v-text">
+      <p v-text="description"></p>
+    </div>
+    <div id="div_v-html">
+      <p>You can learn more in <i v-html="textHtml"></i></p>
+    </div>
+    <div id="div_v-model">
+      <input type="text" v-model="inputText" placeholder="write here">
+      <p>The message is --> {{inputText}}</p>
+      <select v-model="selected">
+        <option disabled value="">Seleccione un elemento</option>
+        <option>A</option>
+        <option>B</option>
+        <option>C</option>
+      </select>
+      <p>You select: {{ selected }}</p>
+    </div>
+    <div id="div_v-bind">
+      <img :src="img" alt=""> <!-- : = v-bind: -->
+      <h2 :class="nameClass">This text should be red</h2>
+    </div>
   </div>
 </template>
 
@@ -14,24 +30,26 @@ export default{
   name:"App", 
   data(){
     return{
-      mensaje: "Hola Vue!",
-      texto: "Vue Framework",
-      textoHtml: '<a href="https://es.vuejs.org">Vue (es)</a>',
-      inputText: 'Escribe un texto',
+      message: "Hello Vue!",
+      description: "Vue is a JavaScript framework for building user interfaces.",
+      textHtml: '<a href="https://es.vuejs.org">vueJS</a>',
+      inputText: '',
+      selected: '',
       img: 'https://picsum.photos/500/300',
-      clasePruebas: "texto-rojo"
+      nameClass: "texto-rojo"
     }
   }
 }
 </script>
 
 <style>
-.texto-azul{
-  color: blue;
-}
 
 .texto-rojo{
   color: red;
+}
+
+.div-content{
+  text-align: center;
 }
 
 </style>
